a1c303d28ba359a8b165ac004f31ff331ecfc982
koreaygj <ygj1828@gmail.com>
2024-12-02 17:38:39 +0900

    chore: 헤더 문제 처리

---
+++ .qt/QtDeployTargets.cmake
@@ -0,0 +1,6 @@
+set(__QT_DEPLOY_TARGET_app_FILE /Users/gyeongjinyang/Desktop/project/school/TTD/packet-eye/src/app/libapp.a)
+set(__QT_DEPLOY_TARGET_app_TYPE STATIC_LIBRARY)
+set(__QT_DEPLOY_TARGET_core_FILE /Users/gyeongjinyang/Desktop/project/school/TTD/packet-eye/src/core/libcore.a)
+set(__QT_DEPLOY_TARGET_core_TYPE STATIC_LIBRARY)
+set(__QT_DEPLOY_TARGET_packet-eye_FILE /Users/gyeongjinyang/Desktop/project/school/TTD/packet-eye/packet-eye)
+set(__QT_DEPLOY_TARGET_packet-eye_TYPE EXECUTABLE)
